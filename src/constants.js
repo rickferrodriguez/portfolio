c58688c75a02fd4f4d727b48ffd74f97de835e3e
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-03-10 10:44:49 -0500

    eslint

--- src/constants.js
+++ src/constants.js
@@ -1,109 +1,108 @@
 export const PROJECTS = [
   {
-    id:1,
+    id: 1,
     name: 'Tic Tac Toe Game',
     img: './tic-tac-toe.png',
     href: 'https://rickferrodriguez.github.io/Tic-Tac-Toe/',
-    repo:'https://github.com/rickferrodriguez/Cursos-React/tree/main/02-tic-tac-toe'
+    repo: 'https://github.com/rickferrodriguez/Cursos-React/tree/main/02-tic-tac-toe'
   },
   {
-    id:2,
+    id: 2,
     name: 'Twitter Card',
     img: './twitter.png',
     href: '',
-    repo:'https://github.com/rickferrodriguez/Cursos/tree/master/React/00-hola-mundo'
+    repo: 'https://github.com/rickferrodriguez/Cursos/tree/master/React/00-hola-mundo'
   },
   {
-    id:3,
+    id: 3,
     name: 'Password Generator',
     img: './passwordgen.png',
     href: 'https://rickferrodriguez.github.io/Desafios/JavaClass/passGenerator/index.html',
-    repo:'https://github.com/rickferrodriguez/Desafios/tree/master/JavaClass'
+    repo: 'https://github.com/rickferrodriguez/Desafios/tree/master/JavaClass'
   },
   {
-    id:4,
+    id: 4,
     name: 'Demo Presupuestos',
     img: './presupuesto.png',
     href: 'https://rickferrodriguez.github.io/Desafios/JavaClass/Presupuesto/index.html',
-    repo:'https://github.com/rickferrodriguez/Desafios/tree/master/JavaClass'
+    repo: 'https://github.com/rickferrodriguez/Desafios/tree/master/JavaClass'
   },
 
   {
-    id:5,
+    id: 5,
     name: 'Demo Simple BlackJack',
     img: './simpleblack.png',
     href: 'https://rickferrodriguez.github.io/Desafios/JavaClass/blackjack/index.html',
-    repo:'https://github.com/rickferrodriguez/Desafios/tree/master/JavaClass'
+    repo: 'https://github.com/rickferrodriguez/Desafios/tree/master/JavaClass'
   },
   {
-    id:6,
+    id: 6,
     name: 'FrontEndMentor',
     img: './frontend.png',
     href: 'https://rickferrodriguez.github.io/Desafios/frontEndMentor/product-preview/index.html',
-    repo:'https://github.com/rickferrodriguez/Desafios/tree/master/frontEndMentor'
-  },
+    repo: 'https://github.com/rickferrodriguez/Desafios/tree/master/frontEndMentor'
+  }
 ]
 
 export const KNOWLEDGE = [
   {
-    id:1,
-    nombre: "React JS",
-    temas: "States, Events, Effects",
+    id: 1,
+    nombre: 'React JS',
+    temas: 'States, Events, Effects',
     img: './react-js.png',
     type: 'know'
   },
   {
-    id:2,
-    nombre: "JavaScript",
-    temas: "Events, Math with JavaScript",
+    id: 2,
+    nombre: 'JavaScript',
+    temas: 'Events, Math with JavaScript',
     img: './js.png',
     type: 'know'
   },
   {
-    id:3,
-    nombre: "CSS",
-    temas: "Responsive development",
+    id: 3,
+    nombre: 'CSS',
+    temas: 'Responsive development',
     img: './css.png',
     type: 'know'
   },
   {
-    id:4,
-    nombre: "HTML",
-    temas: "Maintainability",
+    id: 4,
+    nombre: 'HTML',
+    temas: 'Maintainability',
     img: './html-5.png',
     type: 'know'
   },
   {
-    id:5,
-    nombre: "SQL",
-    temas: "Consulting",
+    id: 5,
+    nombre: 'SQL',
+    temas: 'Consulting',
     img: './sql.png',
     type: 'know'
   },
   {
-    id:6,
-    nombre: "Magento",
-    temas: "Web development",
+    id: 6,
+    nombre: 'Magento',
+    temas: 'Web development',
     img: './magento.png',
     type: 'know'
   },
   {
-    id:7,
-    nombre: "Git",
+    id: 7,
+    nombre: 'Git',
     img: './git.png',
     type: 'tool'
   },
   {
-    id:8,
-    nombre: "Neovim",
+    id: 8,
+    nombre: 'Neovim',
     img: './neovim.png',
     type: 'tool'
   },
   {
-    id:9,
-    nombre: "Linux",
+    id: 9,
+    nombre: 'Linux',
     img: './linux.png',
     type: 'tool'
-  },
+  }
 ]
-
