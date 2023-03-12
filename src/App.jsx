617b4e0f630361bec29c452d0820d0b27974f77c
Richard Rodriguez <108311008+rickferrodriguez@users.noreply.github.com>
2023-03-11 19:35:28 -0500

    Revisar CV

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App () {
           href={pdfPortfolio}
           classN='portfolio-icon'
         />
-        <a href={pdfPortfolio} target='_blank' className='portfolio-text' rel='noreferrer'>Descargar CV</a>
+        <a href={pdfPortfolio} target='_blank' className='portfolio-text' rel='noreferrer'>Revisar CV</a>
       </div>
       <main>
         <section className='home-main-content'>
