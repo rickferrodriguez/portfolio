8a18d35f9dc69bf2d37bcc06c4075f5c8ffbd540
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-03-26 23:46:13 -0500

    my name

--- src/components/home/MainContent.jsx
+++ src/components/home/MainContent.jsx
@@ -1,21 +1,34 @@
 import { Image } from '../header/Header.jsx'
-import myPicture from '/me.png'
+import myPicture from '../../../public/me.png'
+
+const urlLinkedin = 'https://www.linkedin.com/in/richrodriguezdev/'
 
 export function MainContent ({ imgUrl }) {
   return (
-    <section className='home-main-content'>
-      <div className='home-text'>
-        <h1 className='main-h1'>Hi I'm <a href='https://www.linkedin.com/in/richard-rodriguez-13a334267/' target='_blank' className='myName' rel='noreferrer'>RickFeR</a> <span className='emoji-header'>🏋🏼‍♂️</span></h1>
-        <p className='home-parragrafh'>+2 years of experience as a
-          developer. Delivering premium digital solutions. Achieving
-          excellent user experience through performance and usability.
-          Providing scalable and customized web solutions.
-        </p>
-        <section className='social-section'>
-          {
-            imgUrl.map(img => (
+    <section className=''>
+      <h1 className='text-5xl flex gap-3'>
+        Hi I'm
+        <a
+          href={urlLinkedin} target='_blank' rel='noreferrer'
+          className='no-underline'
+        >
+          RickFeR
+        </a>
+        <span className='emoji-header'>🏋🏼‍♂️</span>
+      </h1>
+      <p className='home-parragrafh'>+2 years of experience as a
+        developer. Delivering premium digital solutions. Achieving
+        excellent user experience through performance and usability.
+        Providing scalable and customized web solutions.
+      </p>
+      <ul className='social-section'>
+        {
+          imgUrl.map(img => (
+            <li
+              className='list-none'
+              key={img.id}
+            >
               <a
-                key={img.id}
                 className='social-header-link'
                 href={img.html}
                 target='_blank' rel='noreferrer'
@@ -29,11 +42,11 @@ export function MainContent ({ imgUrl }) {
                   {img.title}
                 </span>
               </a>
-            )
-            )
+            </li>
+          )
+          )
           }
-        </section>
-      </div>
+      </ul>
       <div className='img-avatar-container'>
         <picture>
           <img className='avatar' src={myPicture} alt='Profile Picture' />
