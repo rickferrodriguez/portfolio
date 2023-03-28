9ba578c9aec76a65347b9652f1dc8033efa48cda
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-03-28 13:10:57 -0500

    card clickable

--- src/components/projects/ProjectCard.jsx
+++ src/components/projects/ProjectCard.jsx
@@ -1,28 +1,28 @@
 export const ProjectCard = ({ name, repo, href, img }) => {
   return (
-    <article className='w-full flex flex-col gap-4 justify-center items-center bg-sumiInko1 rounded-[1rem]'>
-      <a className='max-w-[330px] flex justify-center overflow-hidden rounded-t-[1rem]' href={href} target='_blank' rel='noreferrer'>
+    <a className='max-w-[330px] flex justify-center overflow-hidden rounded-t-[1rem] no-underline' href={href} target='_blank' rel='noreferrer'>
+      <article className='w-full flex flex-col gap-4 justify-center items-center bg-sumiInko1 rounded-[1rem]'>
         <img
           className='w-[400px] aspect-[4/3] object-contain '
           src={img} alt={name}
         />
-      </a>
-      <h3 className='text-lg text-center font-bold text-oldWhite m-1'>{name}</h3>
-      <section className='flex gap-2 justify-center'>
-        {[
-          ['View code', repo],
-          ['Visite site', href]
-        ].map(([title, ref], index) => (
-          <a
-            key={index}
-            className='text-sumiInko bg-oldWhite rounded-[1rem] py-1 px-4 mb-6 no-underline
+        <h3 className='text-lg text-center font-bold text-oldWhite m-1'>{name}</h3>
+        <section className='flex gap-2 justify-center'>
+          {[
+            ['View code', repo],
+            ['Visite site', href]
+          ].map(([title, ref], index) => (
+            <a
+              key={index}
+              className='text-sumiInko bg-oldWhite rounded-[1rem] py-1 px-4 mb-6 no-underline
               hover:bg-boatYellow'
-            href={ref} target='_blank' rel='noreferrer'
-          >
-            {title}
-          </a>
-        ))}
-      </section>
-    </article>
+              href={ref} target='_blank' rel='noreferrer'
+            >
+              {title}
+            </a>
+          ))}
+        </section>
+      </article>
+    </a>
   )
 }
